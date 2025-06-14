395dfc1746a487682c88cbaeff242f383ea4c45c
ondarigidion <gidion.ondari@student.moringaschool.com>
2025-06-14 18:31:16 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Declare customerName in the global scope
+var customerName = 'bob';
+
+// Function to upper-case customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Declare bestCustomer without using var/let/const 
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant and attempt illegal reassignment
+const leastFavoriteCustomer = 'someone';
+
+// This will throw an error if actually run
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
+
